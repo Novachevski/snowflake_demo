a46bc8f1dcfdead52d9735a571a3b89eb41d78f0
Borko Novachevski <bnovachevski@ncl.com>
2024-05-14 20:33:38 +0200

    added terraform remote state

--- snowflake_clone/provider.tf
+++ snowflake_clone/provider.tf
@@ -1,9 +1,18 @@
 terraform {
   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.49.0"
+    }
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
       version = "0.90.0"
     }
+  }
+  backend "s3" {
+    bucket = "snowflake-state"
+    key    = "terraform_state"
+    region = "us-east-2"
   }
 }
 
