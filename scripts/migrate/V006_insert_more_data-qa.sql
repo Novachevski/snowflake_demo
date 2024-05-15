ae9037a9e59efd76c2251b34f435a26961e604d0
Borko Novachevski <bnovachevski@ncl.com>
2024-05-15 17:41:12 +0200

    added sql lint

--- scripts/migrate/V006_insert_more_data-qa.sql
+++ scripts/migrate/V006_insert_more_data-qa.sql
@@ -1,4 +1,18 @@
 USE DATABASE SF_TUTS_QA;
-INSERT INTO emp_basic VALUES
-   ('Clementine','Adamou','cadamou@sf_tuts.com','10510 Sachs Road','Klenak','2017-9-22') ,
-   ('Marlowe','De Anesy','madamouc@sf_tuts.co.uk','36768 Northfield Plaza','Fangshan','2017-1-26');
+INSERT INTO EMP_BASIC VALUES
+(
+    'Clementine',
+    'Adamou',
+    'cadamou@sf_tuts.com',
+    '10510 Sachs Road',
+    'Klenak',
+    '2017-9-22'
+),
+(
+    'Marlowe',
+    'De Anesy',
+    'madamouc@sf_tuts.co.uk',
+    '36768 Northfield Plaza',
+    'Fangshan',
+    '2017-1-26'
+);
