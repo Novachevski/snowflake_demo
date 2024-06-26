8b40cb9e2ace8f02212b0f741b9615d87bedb13e
Borko Novachevski <bnovachevski@ncl.com>
2024-06-26 13:57:29 +0200

    testing poc on new account

--- runner.sh
+++ runner.sh
@@ -2,11 +2,11 @@
 #prod ke ima podatoci poveke za da probame diff tool
 #ova e scenario koe ke pokazva diff na fajlovi da se vidi dali dev i prod se synced
 
-for file in "scripts/base/dev"/*; do
-	# Perform an action on each file here
-	echo "Processing file: $file"
-	snowsql -a AUFDSKR-SO14294 -u novachevski -f $file
-done
+# for file in "scripts/base/dev"/*; do
+# 	# Perform an action on each file here
+# 	echo "Processing file: $file"
+# 	snowsql -a AUFDSKR-SO14294 -u novachevski -f $file
+# done
 
 for file in "scripts/base/prod"/*; do
 	# Perform an action on each file here
